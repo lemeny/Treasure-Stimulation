bc6220194c7cc6256df85cf4302711f719965538
lemeny <lemeny@live.com>
2024-08-04 23:17:01 +0800

    update
    delete getnextstate, add a condition of insights for identification

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var startState;
+var IdentifyTreasure;
 var CrossRode;
 var leftPath;
 var rightPath;
@@ -9,6 +10,7 @@ var State1_2;
 var EndGame;
 function initializeStates() {
     startState = { story: "", choices: [] };
+    IdentifyTreasure = { story: "开始鉴定宝物", choices: [] };
     CrossRode = { story: "", choices: [] };
     leftPath = { story: "", choices: [] };
     rightPath = { story: "", choices: [] };
@@ -19,68 +21,67 @@ function initializeStates() {
     EndGame = { story: "游戏结束", choices: [] };
     startState.story = "你是个古玩爱好者，希望通过自己的经验找到一些宝贝";
     startState.choices = [
-        { text: "去古玩街", nextState: [CrossRode], probabilities: [1.0] },
-        { text: "在周围逛逛", nextState: [rightPath], probabilities: [1.0] }
+        { text: "去古玩街", nextState: function () { return CrossRode; } },
+        { text: "在周围逛逛", nextState: function () { return rightPath; } },
+        { text: "把玩自己的藏品", nextState: function (attributes) { return handleIdentifyEvent(attributes); } },
+        { text: "放弃收藏爱好", nextState: function () { return EndGame; } },
     ];
     CrossRode.story = "你来到了一个十字路口";
     CrossRode.choices = [
-        { text: "往左走", nextState: [leftPath], probabilities: [1.0] },
-        { text: "往右走", nextState: [rightPath], probabilities: [1.0] }
+        { text: "往左走", nextState: function () { return leftPath; } },
+        { text: "往右走", nextState: function () { return rightPath; } }
     ];
     leftPath.story = "有一个老大爷在摆地摊";
     leftPath.choices = [
-        { text: "上去看看", nextState: [State1], probabilities: [1.0] },
-        { text: "走开", nextState: [State2], probabilities: [1.0] }
+        { text: "上去看看", nextState: function () { return State1; } },
+        { text: "走开", nextState: function () { return State2; } }
     ];
     rightPath.story = "有个面色不善的小混混盯着你看";
     rightPath.choices = [
-        { text: "加速往前走", nextState: [CrossRode], probabilities: [1.0] },
-        { text: "盯着走上去", nextState: [startState], probabilities: [1.0] }
+        { text: "加速往前走", nextState: function () { return CrossRode; } },
+        { text: "盯着走上去", nextState: function () { return startState; } }
     ];
     State1.story = "老大爷看到你，说：这个古董是我从老家带来的，你看看吧";
     State1.choices = [
-        { text: "看看", nextState: [State1_1], probabilities: [1.0] },
-        { text: "不感兴趣", nextState: [startState], probabilities: [1.0] }
+        { text: "看看", nextState: function () { return State1_1; } },
+        { text: "不感兴趣", nextState: function () { return startState; } }
     ];
     State1_1.story = "你看到了一个古董，你想要买吗？";
     State1_1.choices = [
-        { text: "买", nextState: [State1_2], probabilities: [1.0], condition: function (attributes) { return attributes.money >= 10; } },
-        { text: "不买", nextState: [CrossRode], probabilities: [1.0] }
+        { text: "买", nextState: function () { return State1_2; }, condition: function (attributes) { return attributes.money >= 10; } },
+        { text: "不买", nextState: function () { return CrossRode; } }
     ];
     State1_2.story = "你根据自己的经验买了一件古董";
     State1_2.choices = [
-        { text: "字画", nextState: [startState], probabilities: [1.0] },
-        { text: "瓷器", nextState: [startState], probabilities: [1.0] },
-        { text: "木雕", nextState: [startState], probabilities: [1.0] },
-        { text: "铜器", nextState: [startState], probabilities: [1.0] },
-        { text: "玉器", nextState: [startState], probabilities: [1.0] },
-        { text: "其他", nextState: [startState], probabilities: [1.0] },
+        { text: "字画", nextState: function () { return startState; } },
+        { text: "瓷器", nextState: function () { return startState; } },
+        { text: "木雕", nextState: function () { return startState; } },
+        { text: "铜器", nextState: function () { return startState; } },
+        { text: "玉器", nextState: function () { return startState; } },
+        { text: "其他", nextState: function () { return startState; } }
     ];
     State2.story = "老大爷看到你，说：这个古董是我从老家带来的，你看看吧";
     State2.choices = [
-        { text: "看看", nextState: [State1_1], probabilities: [1.0] },
-        { text: "不感兴趣", nextState: [startState], probabilities: [1.0] }
+        { text: "看看", nextState: function () { return State1_1; } },
+        { text: "不感兴趣", nextState: function () { return startState; } }
     ];
 }
-function getNextState(choice, attributes) {
-    if (choice.condition && !choice.condition(attributes)) {
-        return EndGame;
+function handleIdentifyEvent(attributes) {
+    var successRate = 0.2 + Math.log(attributes.insight);
+    var isSuccess = Math.random() < successRate;
+    if (isSuccess) {
+        attributes.insight += 5;
+        return { story: "鉴定成功！", choices: [{ text: "继续", nextState: function () { return startState; } }] };
     }
-    var nextState;
-    var random = Math.random();
-    var cumulativeProbability = 0;
-    for (var i = 0; i < choice.probabilities.length; i++) {
-        cumulativeProbability += choice.probabilities[i];
-        if (random < cumulativeProbability) {
-            return nextState = choice.nextState[i];
-        }
+    else {
+        attributes.insight += 1;
+        return { story: "鉴定失败！", choices: [{ text: "继续", nextState: function () { return startState; } }] };
     }
-    return choice.nextState[choice.nextState.length - 1];
 }
 var Game = /** @class */ (function () {
     function Game(initialState) {
         this.currentState = initialState;
-        this.playerAttributes = { money: 100, health: 100, reputation: 0 };
+        this.playerAttributes = { money: 100, health: 100, reputation: 0, insight: 0 };
         this.attributesElement = document.getElementById('attributes');
         this.storyElement = document.getElementById('story');
         this.choicesElement = document.getElementById('choices');
@@ -88,7 +89,7 @@ var Game = /** @class */ (function () {
     }
     Game.prototype.render = function () {
         var _this = this;
-        this.attributesElement.innerText = " 金钱：" + this.playerAttributes.money + " 健康：" + this.playerAttributes.health + " 声望：" + this.playerAttributes.reputation;
+        this.attributesElement.innerText = " 金钱：" + this.playerAttributes.money + " 健康：" + this.playerAttributes.health + " 声望：" + this.playerAttributes.reputation + " 见识：" + this.playerAttributes.insight;
         this.storyElement.innerText = this.currentState.story;
         this.choicesElement.innerHTML = '';
         this.currentState.choices.forEach(function (choice) {
@@ -101,13 +102,11 @@ var Game = /** @class */ (function () {
         });
     };
     Game.prototype.handleChoice = function (choice) {
-        // 添加条件判断和角色属性更新逻辑
-        var nextState = getNextState(choice, this.playerAttributes);
-        this.currentState = nextState;
-        // 这里可以更新角色属性，比如减少金钱
-        if (nextState === State1_2) {
-            this.playerAttributes.money -= 10; // 假设购买古董需要花费10金钱
+        if (choice.condition && !choice.condition(this.playerAttributes)) {
+            alert('条件未满足');
+            return;
         }
+        this.currentState = choice.nextState(this.playerAttributes);
         this.render();
     };
     return Game;
--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 interface GameState {
     story: string;
     choices: Choice[];
+    outcomes?: Outcome[];
 }
 
 interface Choice {
     text: string;
-    nextState: GameState[];
-    probabilities: number[];
+    nextState: (attributes:PlayerAttribute) => GameState;
+    // probabilities: number[];
     condition?: (attributes:PlayerAttribute) => boolean;
 
 }
@@ -43,79 +44,77 @@ function initializeStates() {
     State2 = { story: "", choices: [] };
     State1_1 = { story: "", choices: [] };
     State1_2 = { story: "", choices: [] };
-    EndGame = { story: "游戏结束", choices: [] };
+    EndGame = { story: "看来收藏古玩不适合你，让我们看看喜欢收藏的世界线吧！", choices: [] };
 
     startState.story = "你是个古玩爱好者，希望通过自己的经验找到一些宝贝";
     startState.choices = [
-        { text: "去古玩街", nextState: [CrossRode] , probabilities: [1.0] },
-        { text: "在周围逛逛", nextState: [rightPath], probabilities: [1.0] },
-        { text: "把玩自己的藏品", nextState: [IdentifyTreasure], probabilities: [1.0] },
-        { text: "放弃收藏爱好", nextState: [EndGame], probabilities: [1.0] },
+        { text: "去古玩街", nextState: () => CrossRode },
+        { text: "在周围逛逛", nextState: () => rightPath },
+        { text: "把玩自己的藏品", nextState: (attributes) => handleIdentifyEvent(attributes) },
+        { text: "放弃收藏爱好", nextState: () => EndGame },
     ];
 
     CrossRode.story = "你来到了一个十字路口";
     CrossRode.choices = [
-        { text: "往左走", nextState: [leftPath] , probabilities: [1.0] },
-        { text: "往右走", nextState: [rightPath] , probabilities: [1.0] }
+        { text: "往左走", nextState: () => leftPath },
+        { text: "往右走", nextState: () => rightPath }
     ];
 
     leftPath.story = "有一个老大爷在摆地摊";
     leftPath.choices = [
-        { text: "上去看看", nextState: [State1], probabilities: [1.0] },
-        { text: "走开", nextState: [State2], probabilities: [1.0] }
+        { text: "上去看看", nextState: () => State1 },
+        { text: "走开", nextState: () => State2 }
     ];
 
     rightPath.story = "有个面色不善的小混混盯着你看";
     rightPath.choices = [
-        { text: "加速往前走", nextState: [CrossRode], probabilities: [1.0] },
-        { text: "盯着走上去", nextState: [startState], probabilities: [1.0] }
+        { text: "加速往前走", nextState: () => CrossRode },
+        { text: "盯着走上去", nextState: () => startState }
     ];
 
     State1.story = "老大爷看到你，说：这个古董是我从老家带来的，你看看吧";
     State1.choices = [
-        { text: "看看", nextState: [State1_1], probabilities: [1.0] },
-        { text: "不感兴趣", nextState: [startState], probabilities: [1.0] }
+        { text: "看看", nextState: () => State1_1 },
+        { text: "不感兴趣", nextState: () => startState }
     ];
 
     State1_1.story = "你看到了一个古董，你想要买吗？";
     State1_1.choices = [
-        { text: "买", nextState: [State1_2], probabilities: [1.0], condition: (attributes) => attributes.money >= 10 },
-        { text: "不买", nextState: [CrossRode], probabilities: [1.0] }
+        { text: "买", nextState: () => State1_2, condition: (attributes) => attributes.money >= 10 },
+        { text: "不买", nextState: () => CrossRode }
     ];
 
     State1_2.story = "你根据自己的经验买了一件古董";
     State1_2.choices = [
-        { text: "字画", nextState: [startState], probabilities: [1.0] },
-        { text: "瓷器", nextState: [startState] , probabilities: [1.0] },
-        { text: "木雕", nextState: [startState] , probabilities: [1.0] },
-        { text: "铜器", nextState: [startState] , probabilities: [1.0] },
-        { text: "玉器", nextState: [startState] , probabilities: [1.0] },
-        { text: "其他", nextState: [startState] , probabilities: [1.0] },
+        { text: "字画", nextState: () => startState },
+        { text: "瓷器", nextState: () => startState },
+        { text: "木雕", nextState: () => startState },
+        { text: "铜器", nextState: () => startState },
+        { text: "玉器", nextState: () => startState },
+        { text: "其他", nextState: () => startState }
     ];
 
     State2.story = "老大爷看到你，说：这个古董是我从老家带来的，你看看吧";
     State2.choices = [
-        { text: "看看", nextState: [State1_1], probabilities: [1.0] },
-        { text: "不感兴趣", nextState: [startState], probabilities: [1.0] }
+        { text: "看看", nextState: () => State1_1 },
+        { text: "不感兴趣", nextState: () => startState }
     ];
 }
 
-function getNextState(choice: Choice, attributes : PlayerAttribute): GameState {
-    if (choice.condition && !choice.condition(attributes)) {
-        return EndGame;
-    }
 
-    let nextState: GameState;
-    const random = Math.random();
-    let cumulativeProbability = 0;
-    for (let i = 0; i < choice.probabilities.length; i++) {
-        cumulativeProbability += choice.probabilities[i];
-        if (random < cumulativeProbability) {
-            return nextState = choice.nextState[i];
-        }
+function handleIdentifyEvent(attributes: PlayerAttribute): GameState {
+    const successRate = 0.2 + Math.log(attributes.insight);
+    const isSuccess = Math.random() < successRate;
+
+    if (isSuccess) {
+        attributes.insight += 5;
+        return { story: "鉴定成功！", choices: [{ text: "继续", nextState: () => startState }] };
+    } else {
+        attributes.insight += 1;
+        return { story: "鉴定失败！", choices: [{ text: "继续", nextState: () => startState }] };
     }
-    return choice.nextState[choice.nextState.length - 1];
 }
+
 
 class Game {
     private currentState: GameState;
@@ -126,7 +125,7 @@ class Game {
 
     constructor(initialState: GameState) {
         this.currentState = initialState;
-        this.playerAttributes = { money: 100, health: 100, reputation: 0 };
+        this.playerAttributes = { money: 100, health: 100, reputation: 0 ,insight: 0};
         this.attributesElement = document.getElementById('attributes')!;
         this.storyElement = document.getElementById('story')!;
         this.choicesElement = document.getElementById('choices')!;
@@ -134,7 +133,7 @@ class Game {
     }
 
     private render() {
-        this.attributesElement.innerText = " 金钱：" + this.playerAttributes.money + " 健康：" + this.playerAttributes.health + " 声望：" + this.playerAttributes.reputation;
+        this.attributesElement.innerText = " 金钱：" + this.playerAttributes.money + " 健康：" + this.playerAttributes.health + " 声望：" + this.playerAttributes.reputation+ " 见识：" + this.playerAttributes.insight;
         this.storyElement.innerText = this.currentState.story;
         this.choicesElement.innerHTML = '';
         this.currentState.choices.forEach(choice => {
@@ -148,15 +147,12 @@ class Game {
     }
 
     private handleChoice(choice: Choice) {
-        // 添加条件判断和角色属性更新逻辑
-        const nextState = getNextState(choice,this.playerAttributes);
-        this.currentState = nextState;
-
-        // 这里可以更新角色属性，比如减少金钱
-        if (nextState === State1_2) {
-            this.playerAttributes.money -= 10; // 假设购买古董需要花费10金钱
+        if (choice.condition && !choice.condition(this.playerAttributes)) {
+            alert('条件未满足');
+            return;
         }
 
+        this.currentState = choice.nextState(this.playerAttributes);
         this.render();
     }
 }
